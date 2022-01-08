8d2e00d20dfcf9643149245ed884d9b7fa7724a6
Rhaka Novyansyah Dewanto <rhakadewanto26@gmail.com>
2022-01-08 22:16:36 +0700

    add tailwind and ant Icon

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extends: {},
+    colors: {
+      primary: '#F90716',
+      accent: '#fa515b',
+      secondary: '#FFF000',
+      white: '#FFFFFF',
+    }
+  },
+  plugins: [],
+}
